0b3b467235be13a2c38fa56b511fd1788a8b2ad3
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-11-03 19:08:13 -0500

    ahora si, experience page done

--- components/Experience.js
+++ components/Experience.js
@@ -2,11 +2,7 @@ import React from "react";
 import styles from "../styles/Experience.module.css";
 import { es, en } from "../public/lang/language"
 
-function Experience({isEnglish, setIsEnglish}) {
-  const toggleLang = () => {
-    setIsEnglish(!isEnglish);
-  }
-
+function Experience({isEnglish}) {
   return(
     <div className={styles.container} id="experience">
       <h1 className={styles.title}>{isEnglish ? en.menu.experience : es.menu.experience}</h1>
@@ -14,7 +10,7 @@ function Experience({isEnglish, setIsEnglish}) {
           <h2 className={styles.infoTitle}>{isEnglish ? en.experience.labsti.job : es.experience.labsti.job} <span>@{isEnglish ? en.experience.labsti.name : es.experience.labsti.name}</span></h2>
           <span className={styles.date}>{isEnglish ? en.experience.labsti.date : es.experience.labsti.date}</span>
           <ul className={styles.toDoList}>
-            {isEnglish ? en.experience.labsti.toDo.map((item) => (<li>{item}</li>)): es.experience.labsti.toDo.map((item) => (<li>{item}</li>))}
+            {isEnglish ? en.experience.labsti.toDo.map((item, index) => (<li key={index}>{item}</li>)): es.experience.labsti.toDo.map((item, index) => (<li key={index}>{item}</li>))}
           </ul>
       </div>
     </div>
