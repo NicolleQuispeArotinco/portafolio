d62f2f104c29f44a88b630dd00867b2add13afb9
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-11-03 19:25:13 -0500

    principal page done

--- components/Principal.js
+++ components/Principal.js
@@ -1,34 +1,27 @@
-import React, {useState} from "react";
+import React from "react";
 import styles from "../styles/Main.module.css"
-import { Button, IconButton, Link } from "@chakra-ui/core";
+import { Button } from "@chakra-ui/core";
 import { es, en } from "../public/lang/language"
 
-function Principal({isEnglish, setIsEnglish}) {
-  const toggleLang = () => {
-    setIsEnglish(!isEnglish);
-  }
-
+function Principal({isEnglish}) {
   return(
-    // <div className={styles.principal}>
-      <div className={styles.mainInfo}>
-        <h3 className={styles.hello}>
-          {isEnglish ? en.greeting: es.greeting}
-        </h3>
-        <h1 className={styles.name}>
-          Nicolle Quispe.
-        </h1>
-        <h2 className={styles.occupation}>
-          {isEnglish ? en.occupation: es.occupation}
-        </h2>
-        <a href="#about">
-          <Button variantColor={"coral"} variant="outline" className={styles.viewButton}>
-            {isEnglish ? en.viewButton: es.viewButton}
-            <img src="/images/arrow.svg"/>
-          </Button>
-          </a>
-      </div>
-      
-    // </div>
+    <div className={styles.mainInfo}>
+      <h3 className={styles.hello}>
+        {isEnglish ? en.greeting: es.greeting}
+      </h3>
+      <h1 className={styles.name}>
+        Nicolle Quispe.
+      </h1>
+      <h2 className={styles.occupation}>
+        {isEnglish ? en.occupation: es.occupation}
+      </h2>
+      <a href="#about">
+        <Button variantColor={"coral"} variant="outline" className={styles.viewButton}>
+          {isEnglish ? en.viewButton: es.viewButton}
+          <img src="/images/arrow.svg"/>
+        </Button>
+        </a>
+    </div>
   )
 }
 
