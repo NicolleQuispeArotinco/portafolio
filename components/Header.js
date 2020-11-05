454f32612513a3274d0b4253d8cb6da0aa024803
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-11-05 11:30:43 -0500

    add smooth scroll

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/Header.module.css";
 import { Button } from "@chakra-ui/core";
+import {Link} from "react-scroll";
 
 function Header({isEnglish, setIsEnglish, es, en}){
   const toggleLang = () => {
@@ -9,11 +10,11 @@ function Header({isEnglish, setIsEnglish, es, en}){
 
   return(
     <header className={styles.nav}>
-      <ul className={styles.options}>
-        <li><a href="#about">{isEnglish ? en.menu.about : es.menu.about}</a></li>
-        <li><a href="#experience">{isEnglish ? en.menu.experience : es.menu.experience}</a></li>
-        <li><a href="#projects">{isEnglish ? en.menu.projects : es.menu.projects}</a></li>
-      </ul>
+      <div className={styles.options}>
+        <Link to="about" spy={true} smooth={true} offset={0} duration={500} className={styles.option}>{isEnglish ? en.menu.about : es.menu.about}</Link>
+        <Link to="experience" spy={true} smooth={true} offset={0} duration={500} className={styles.option}>{isEnglish ? en.menu.experience : es.menu.experience}</Link>
+        <Link to="projects" spy={true} smooth={true} offset={0} duration={500} className={styles.option}>{isEnglish ? en.menu.projects : es.menu.projects}</Link>
+      </div>
       <div>
         <Button variantColor="white" variant="outline" onClick={toggleLang} className={styles.iconButton}>
           {isEnglish ? "En" : "Es"}
