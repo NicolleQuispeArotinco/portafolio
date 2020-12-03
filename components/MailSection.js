75acabb64fa7d080f59595e22edccb94d80b228a
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-12-03 18:40:33 -0500

    add link to email

--- components/MailSection.js
+++ components/MailSection.js
@@ -5,7 +5,12 @@ function MailSection(){
   return(
     <div className={styles.mailContainer}>
       <div className={styles.textContainer}>
-        <span>nicolleqa03@gmail.com</span>
+        <a
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=nicolleqa03@gmail.com"
+          target="_blank"
+        >
+          nicolleqa03@gmail.com
+        </a>
       </div>
     </div>
   )
